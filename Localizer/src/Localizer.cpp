dca54c7e78f963d2acb028ee30cadfe453d5c312
Şamil Bayyurt <35709507+SamBayy@users.noreply.github.com>
2020-12-23 23:34:57 +0300

    Update Localizer.cpp
    Fingerprint kısmının deneyerek doldurulması lazım. Bir de point type objenin x y değerlerinin nasıl güncellendiğini bulamadım location.x dedim.

--- Localizer/src/Localizer.cpp
+++ Localizer/src/Localizer.cpp
@@ -40,9 +40,75 @@ void Localizer::RemoveTarget(string name)
 
 void Localizer::UpdateLocation()
 {
+	/*
+    x
+    ^
+    .
+    .
+    ESP3
+    .
+    .
+    .
+    ESP1 . . . ESP2. . .>y
+    */
+	double z1;
+    	double z2;
+    	double z3;
+	
+	for (auto it = begin(targets); it != end(targets); ++it) {
+		z1 = RSSI_to_Meter(it->rssi);
+	}
+	
+	z1 = z1*z1;
+	z2 = z2*z2;
+	z3 = z3*z3;
+	
+	location.x = (z1-z2+9)/6; //for 1 meter square area 6->2, 9->1
+    	location.y = (z1-z3+9)/6; //for 1 meter square area 6->2, 9->1
 }
 
 Point Localizer::GetLocation()
 {
 	return location;
+}
+///////////////////////////////////////////////////// Fingerprint
+double RSSI_to_Meter(double RSSI){
+   double from_source;
+    if(RSSI < 10){
+        //linearization with the remaining values.
+        from_source = 0;
+        return from_source;
+    }
+    else if(RSSI < 20){
+        from_source = 0;
+        return from_source;
+    }
+    else if(RSSI < 30){
+        from_source = 0;
+        return from_source;
+    }
+    else if(RSSI < 40){
+        from_source = 0;
+        return from_source;
+    }
+    else if(RSSI < 50){
+        from_source = 0;
+        return from_source;
+    }
+    else if(RSSI < 60){
+        from_source = 0;
+        return from_source;
+    }
+    else if(RSSI < 70){
+        from_source = 0;
+        return from_source;
+    }
+    else if(RSSI < 80){
+        from_source = 0;
+        return from_source;
+    }
+    else{
+        from_source = 0;
+        return from_source;
+    }
 }
